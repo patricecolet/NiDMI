b59db010ef21eac1cce5c6322de785e808ba0b0f
nurulbintifadzil <innsyirah.bfadzil@gmail.com>
2026-03-11 11:37:43 +0100

    Adding WSL for Arduino

--- scripts/nidmi.sh
+++ scripts/nidmi.sh
@@ -29,15 +29,21 @@ MAC_LIB_DEFAULT="$HOME/Documents/Arduino/libraries/NiDMI"
 MAC_CACHE_DEFAULT="$HOME/Library/Caches/arduino/sketches"
 LINUX_LIB_DEFAULT="$HOME/Arduino/libraries/NiDMI"
 LINUX_CACHE_DEFAULT="$HOME/.arduino15/sketches"
+WSL_LIB_DEFAULT="mnt/c/Users/Public/Documents/Arduino/libraries/NiDMI"
+WSL_CACHE_DEFAULT="mnt/c/Users/Public/AppData/Local/Arduino15/sketches"
 
 case "$PLATFORM" in
     mac)
         ARDUINO_LIB_DIR="${ARDUINO_LIB_DIR:-$MAC_LIB_DEFAULT}"
         ARDUINO_CACHE_DIR="${ARDUINO_CACHE_DIR:-$MAC_CACHE_DEFAULT}"
         ;;
-    wsl|linux)
+    linux)
         ARDUINO_LIB_DIR="${ARDUINO_LIB_DIR:-$LINUX_LIB_DEFAULT}"
         ARDUINO_CACHE_DIR="${ARDUINO_CACHE_DIR:-$LINUX_CACHE_DEFAULT}"
+        ;;
+    wsl)
+        ARDUINO_LIB_DIR="${ARDUINO_LIB_DIR:-$WSL_LIB_DEFAULT}"
+        ARDUINO_CACHE_DIR="${ARDUINO_CACHE_DIR:-$WSL_CACHE_DEFAULT}"
         ;;
 esac
 
